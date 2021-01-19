7817cebb978309750349b54bd11a96dc4499a8f8
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-01-19 01:50:37 -0800

    the protflio is completed

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+      $('.navbar').addClass('sticky')
+      $('.navbar .max-width .logo a .port').addClass('sticky')
+    }else{
+      $('.navbar').removeClass('sticky')
+      $('.navbar .max-width .logo a .port').removeClass('sticky')
+    }
+  })
+
+  $('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass('active')
+    $('.menu-btn i').toggleClass('active')
+
+  });
+
+  let typed  = new Typed(".typing", {
+    strings:['Software Developer', 'Web Developer'],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+  })
+
+   $('.carousel').owlCarousel({
+     margin:20,
+     loop:true,
+     autoplayTimeOut:2000,
+     autoplayHoverPause:true,
+     responsive:{
+       0:{
+         items:1,
+         nav:false
+       },
+       600:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:3  ,
+        nav:false
+      }
+     }
+
+   });
+})
